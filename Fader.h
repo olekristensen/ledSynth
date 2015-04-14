8d931f36431d2e02440298b6d9df5e1d758a084b
Ole Kristensen <ole@kristensen.name>
2015-04-14 16:53:46 +0200

    Protocol added

---
+++ Fader.h
@@ -0,0 +1,165 @@
+#include "Arduino.h"
+
+class Fader {
+  public:
+    Adafruit_DCMotor *_motor;
+    Adafruit_ADS1115 *_ads;
+    int _adsFaderPosChannel;
+    PID *_pid;
+    long _millisLastFade = 0;
+    double _setpoint = 0;
+    double _pos = 0;
+    double _speed = 0;
+    double _outputLimit = 0;
+
+    int16_t _faderAdsValue = 0;
+    int16_t _faderCalibrationHigh = 17100;
+    int16_t _faderCalibrationLow = 20;
+
+    const int16_t faderPadding = 400;
+
+    double potRangeFrom;
+    double potIRangeTo;
+
+    enum State { F_MANUAL, F_AUTOMATIC };
+    int _state = F_AUTOMATIC;
+
+    Fader(Adafruit_DCMotor *motor, Adafruit_ADS1115 *ads, int adsFaderPosChannel, double pidP, double pidI, double pidD, double sampleRate, double setpoint = 0, double spd = 0) {
+      _motor = motor;
+      _ads = ads;
+      _adsFaderPosChannel = adsFaderPosChannel;
+      _setpoint = setpoint;
+      _speed = spd;
+      _pid = new PID(&_pos, &_speed, &_setpoint, pidP, pidI, pidD, DIRECT);
+      _pid->SetSampleTime(sampleRate); // Sets the sample rate
+      _pid->SetMode(AUTOMATIC);
+    }
+
+    void setSpeedLimits(double outputLimit) {
+      _outputLimit = outputLimit;
+      _pid->SetOutputLimits(0.0 - outputLimit, outputLimit); // Set max speed for DC motors
+    }
+
+    void setAdsGain(adsGain_t gain) {
+      _ads->setGain(gain);    // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+    }
+
+    void begin() {
+      _ads->begin();
+    }
+
+    void setSetpoint(double setpoint) {
+      _setpoint = setpoint;
+    }
+
+    void setSetpointNormalised(double setpoint) {
+      setSetpoint(setpoint * 1023.0);
+    }
+
+    double getSetpoint() {
+      return _setpoint;
+    }
+
+    double getSetpointNormalised() {
+      return getSetpoint() / 1023.0;
+    }
+
+    double getPos() {
+      return _pos;
+    }
+
+    int16_t getAdsValue(int adsChannel) {
+      return _ads->readADC_SingleEnded(adsChannel);
+    }
+
+    int16_t getLastFaderAdsValue() {
+      return _faderAdsValue;
+    }
+
+    void setManual() {
+      if (_state != F_MANUAL) {
+        _state = F_MANUAL;
+        _pid->SetMode(MANUAL);
+      }
+    }
+
+    void setAutomatic() {
+      if (_state != F_AUTOMATIC) {
+        _state = F_AUTOMATIC;
+        _pid->SetMode(AUTOMATIC);
+      }
+    }
+
+    void update() {
+      _faderAdsValue = getAdsValue(_adsFaderPosChannel);
+      _pos = constrain(mapFloat(_faderAdsValue, _faderCalibrationLow, _faderCalibrationHigh, 0.0, 1023.0), 0.0, 1023.0);
+      if (_state == F_AUTOMATIC) {
+        if (_pid->Compute()) {
+          _motor->setSpeed(floor(abs(_speed)));
+          if (abs(_pos - _setpoint) < 1.0 ) {
+            _motor->run(RELEASE);
+          } else if (_speed < 0.0) {
+            if (_pos > 0.0) {
+              _motor->run(BACKWARD);
+            } else {
+              _motor->run(RELEASE);
+            }
+          } else {
+            if (_pos < 1023.0) {
+              _motor->run(FORWARD);
+            } else {
+              _motor->run(RELEASE);
+            }
+          }
+        }
+      } else if (_state == F_MANUAL) {
+        _motor->run(RELEASE);
+        _setpoint = _pos;
+      }
+    }
+
+    void calibrate() {
+      _pid->SetMode(MANUAL);
+      int16_t maxAdsValue = 0;
+      int16_t minAdsValue = 17000;
+      int16_t newAdsValue = 0;
+      long lastTestMillis = 0;
+      const int millisEndWindow = 150;
+      const int minOutput = _outputLimit / 6;
+
+      Serial.println("CALIBRATING");
+
+      // find high ads value
+      _motor->run(FORWARD);
+      lastTestMillis = millis();
+      while (millis() - lastTestMillis < millisEndWindow) {
+        newAdsValue = getAdsValue(_adsFaderPosChannel);
+        if (newAdsValue > maxAdsValue) {
+          maxAdsValue = max(maxAdsValue, newAdsValue);
+          _motor->setSpeed(min(_outputLimit, map(maxAdsValue, minAdsValue, minAdsValue / 2, minOutput, _outputLimit)));
+          lastTestMillis = millis();
+        }
+      }
+
+      // find low ads value
+      _motor->setSpeed(_outputLimit);
+      _motor->run(BACKWARD);
+      lastTestMillis = millis();
+      while (millis() - lastTestMillis < millisEndWindow) {
+        newAdsValue = getAdsValue(_adsFaderPosChannel);
+        if (newAdsValue < minAdsValue) {
+          minAdsValue = min(minAdsValue, newAdsValue);
+          _motor->setSpeed(min(_outputLimit, map(minAdsValue, 0, maxAdsValue / 2, minOutput, _outputLimit)));
+          lastTestMillis = millis();
+        }
+      }
+
+      _faderCalibrationLow = minAdsValue + faderPadding;
+      _faderCalibrationHigh = maxAdsValue - faderPadding;
+
+      _motor->run(RELEASE);
+
+      _pid->SetMode(AUTOMATIC);
+    }
+
+};
--- ITUlightBox.ino
+++ ITUlightBox.ino
@@ -1,19 +1,21 @@
 // IDENTITY
 
-const int id = 1;
-const char *idChars = "1";
-const String identityString = "LightSynth";
-const char *identityChars = "LightSynth";
+const int id = 2;
+const char *idChars = "2";
+const String identityString = "LEDSYNTH";
+const char *identityChars = "LEDSYNTH";
 
 const int versionMajor = 0;
 const int versionMinor = 3;
 
-#include <RFduinoBLE.h>
 #include <Wire.h>
 #include <LiquidTWI.h>               // Display
 #include <Adafruit_ADS1015.h>        // Analog Digital Converter
 #include <Adafruit_MotorShield.h>    // Motor Shield
 #include <PID_v1.h>
+#include <RFduinoBLE.h>
+#include <QueueList.h>
+#include <String.h>
 #include "Fader.h"
 
 
@@ -21,15 +23,29 @@ const int versionMinor = 3;
 
 LiquidTWI lcd(0);                     //0x20
 
+byte lcdCharBluetooth[8] = {0x4, 0x16, 0xd, 0x6, 0xd, 0x16, 0x4};
+byte lcdCharFat1[8] = {0x2, 0x6, 0xe, 0x6, 0x6, 0x6, 0x6};
+byte lcdCharFat2[8] = {0xe, 0x1b, 0x3, 0x6, 0xc, 0x18, 0x1f};
+byte lcdCharFat3[8] = {0xe, 0x1b, 0x3, 0xe, 0x3, 0x1b, 0xe};
+byte lcdCharFat4[8] = {0x3, 0x7, 0xf, 0x1b, 0x1f, 0x3, 0x3};
+byte lcdCharFat5[8] = {0x1f, 0x18, 0x1e, 0x3, 0x3, 0x1b, 0xe};
+byte lcdCharFat6[8] = {0xe, 0x1b, 0x18, 0x1e, 0x1b, 0x1b, 0xe};
+byte lcdCharFat7[8] = {0x1f, 0x3, 0x6, 0xc, 0xc, 0xc, 0xc};
+byte lcdCharFat8[8] = {0xe, 0x1b, 0x1b, 0xe, 0x1b, 0x1b, 0xe};
+byte lcdCharFat9[8] = {0xe, 0x1b, 0x1b, 0xf, 0x3, 0x1b, 0xe};
+byte lcdCharFat0[8] = {0xe, 0x1b, 0x1b, 0x1b, 0x1b, 0x1b, 0xe};
+
+byte * lcdCharNumbers[10] = {lcdCharFat0, lcdCharFat1, lcdCharFat2, lcdCharFat3, lcdCharFat4, lcdCharFat5, lcdCharFat6, lcdCharFat7, lcdCharFat8, lcdCharFat9 };
+
 // FADERS
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
 
-const double faderMotorSpeed = 156;
-const int faderMotorHertz = 55;
+const double faderMotorSpeed = 200;//156;
+const int faderMotorHertz = 110 / 2;
 const double faderPidP = .75;
 const double faderPidI = 20;
-const double faderPidD = 0.0002;
+const double faderPidD = 0.002;
 
 const int faderPidSampleRate = 10;                  // Calling compute() every 10 ms.
 
@@ -45,13 +61,30 @@ Fader faderTemperature(
   &faderTemperaturePots, 0,
   faderPidP, faderPidI, faderPidD, faderPidSampleRate);
 
+
 // PWM
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
+
+// BLUETOOTH
+enum bleInputStates { BIS_START, BIS_COMMAND, BIS_DATA, BIS_END };
+int bleInputState = BIS_START;
+char bleInputCommandBuf;
+String bleInputHexstringBuf;
+
+struct bleCommand{
+  char command;
+  String hexstring;
+};
+
+QueueList <bleCommand> bleCommandQueue;
+
+bool bleAdvertising = false;
+
 // CONTROL
 
-enum State { S_SETUP, S_FADER_CALIBRATE, S_PID_TEST, S_RUNNING };
+enum State { S_SETUP, S_FADER_CALIBRATE, S_PID_TEST, S_STANDALONE_SETUP, S_STANDALONE_LOOP, S_CONNECTED_SETUP, S_CONNECTED_LOOP };
 int state = S_SETUP;
 long millisLastFrame = 0;
 long frameCount = 0;
@@ -71,23 +104,26 @@ void setup() {
   Serial.print(versionMinor);
 
   lcd.begin(16, 2);
+
+  lcd.createChar(0, lcdCharBluetooth);   //Bluetooth Icon
+  lcd.createChar(1, lcdCharNumbers[id]); //Fat indentity number
+
   lcd.clear();
   //Print unit info on display
-  lcd.print(identityString);
+  lcd.write(byte(1));
   lcd.print(" ");
-  lcd.print(id);
-  lcd.setCursor(0, 1);
-  lcd.print("ver. ");
+  lcd.print(identityString);
+  lcd.setCursor(2, 1);
+  lcd.print("v.");
   lcd.print(versionMajor);
   lcd.print(".");
   lcd.print(versionMinor);
 
   //Start BLE Advertisement
-  RFduinoBLE.advertisementInterval = 675;
+  RFduinoBLE.advertisementInterval = 200;
   RFduinoBLE.deviceName = identityChars;
   RFduinoBLE.advertisementData = idChars;
   RFduinoBLE.txPowerLevel = +4;
-  RFduinoBLE.begin();
 
   AFMS.begin(faderMotorHertz);  // create with the default frequency 1.6KHz
   faderIntensity.setSpeedLimits(faderMotorSpeed);
@@ -104,25 +140,37 @@ void setup() {
 void loop() {
   long thisFrameMillis = millis();
   double t = thisFrameMillis * 0.001 * 0.5;
+  int fadeSteps = 500;
   switch (state) {
     case S_SETUP :
-      delay(1000);
+      for (int i = 0; i < fadeSteps; i++) {
+        double iNorm = i * 1.0 / fadeSteps * 1.0;
+        // fade up from warm to cold
+        setDisplayColorRGB(iNorm, iNorm * iNorm * iNorm, iNorm * iNorm * iNorm * iNorm);
+        delay(10);
+      }
+      delay(250);
       state = S_FADER_CALIBRATE;
       break;
     case S_FADER_CALIBRATE :
-      lcd.setCursor(0, 1);
-      lcd.print("Calibrating...");
+      state = S_STANDALONE_SETUP;
+      lcd.clear();
+      lcd.write(byte(1));
+      lcd.print(" ");
+      lcd.print("calibrating...");
       faderTemperature.calibrate();
       faderIntensity.calibrate();
-      state = S_PID_TEST;
+      RFduinoBLE.begin();
+      lcd.clear();
       break;
     case S_PID_TEST :
       // FADER TEST
 
+      faderIntensity.setAutomatic();
+      faderTemperature.setAutomatic();
+
       //Intensity
-
       faderIntensity.update();
-
       if (fmod(t, 3.0) < 2.0) {
         faderIntensity.setSetpoint(
           mapFloat(sin(sin(t * 2.0) * fmod(t, 3.0)), -1.0, 1.0, 24.0, 1000.0)
@@ -130,11 +178,8 @@ void loop() {
       }
 
       //Temperature
-
       faderTemperature.update();
-
-      //t += 0.5;
-
+      t += 0.5;
       if (fmod(t, 3.0) < 2.0) {
         faderTemperature.setSetpoint(
           mapFloat(sin(sin(t * 2.0) * fmod(t, 3.0)), -1.0, 1.0, 24.0, 1000.0)
@@ -142,7 +187,6 @@ void loop() {
       }
 
       // Display
-
       lcd.setCursor(0, 0);
       lcdPrintNumberPadded(faderIntensity.getLastFaderAdsValue(), 5, ' ');
       lcdPrintNumberPadded(faderIntensity.getSetpoint(), 5, ' ');
@@ -152,12 +196,201 @@ void loop() {
       lcdPrintNumberPadded(faderTemperature.getSetpoint(), 5, ' ');
       lcdPrintNumberPadded(faderTemperature.getSetpoint() - faderTemperature.getPos() , 5, ' ');
       break;
-    case S_RUNNING  :
-      ;
+    case S_STANDALONE_SETUP :
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.write(byte(1)); // fat id number
+      lcd.print(" ");
+      lcd.print("no connection");
+      for (int i = 0; i < fadeSteps; i++) {
+        double iNorm = i * 1.0 / fadeSteps * 1.0;
+        iNorm = 0.5 + (sin(2.0 * (iNorm + 0.25) * PI) / 2.0);
+        // yellow flash
+        setDisplayColorRGB(1.0, iNorm, iNorm * iNorm);
+        delay(1);
+      }
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.write(byte(1)); // fat id number
+      lcd.print(" ");
+      lcd.print("standalone");
+      faderIntensity.setManual();
+      faderTemperature.setManual();
+      state = S_STANDALONE_LOOP;
       break;
+    case S_STANDALONE_LOOP  :
+      faderIntensity.update();
+      faderTemperature.update();
+
+      // Display
+      lcd.setCursor(15, 0);
+      if (thisFrameMillis % 2000 < 800 && thisFrameMillis % 400 < 200 ) {
+        lcd.write(byte(0));
+      } else {
+        lcd.print(" ");
+      }
+      lcd.setCursor(0, 1);
+      lcdPrintNumberPadded(faderIntensity.getSetpointNormalised() * 100, 5, ' ');
+      lcd.print("%");
+      lcdPrintNumberPadded(faderTemperature.getSetpointNormalised() * 100, 6, ' ');
+      lcd.print("%");
+      /*
+      // millis per frame
+      lcd.setCursor(0, 1);
+      lcdPrintNumberPadded(thisFrameMillis - millisLastFrame, 2, ' ');
+      */
+      break;
+    case S_CONNECTED_SETUP :
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.write(byte(1)); // fat id number
+      lcd.print(" ");
+      lcd.print("connected");
+      lcd.setCursor(15, 0);
+      lcd.write(byte(0)); // bluetooth icon
+      for (int i = 0; i < fadeSteps; i++) {
+        double iNorm = i * 1.0 / fadeSteps * 1.0;
+        iNorm = 0.5 + (sin(2.0 * (iNorm + 0.25) * PI) / 2.0);
+        // yellow flash
+        setDisplayColorRGB(iNorm * iNorm, 1.0, iNorm);
+        delay(1);
+      }
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.write(byte(1)); // fat id number
+      lcd.print(" ");
+      lcd.print("connected");
+      lcd.setCursor(15, 0);
+      lcd.write(byte(0)); // bluetooth icon
+      faderIntensity.setAutomatic();
+      faderTemperature.setAutomatic();
+      state = S_CONNECTED_LOOP;
+      break;
+    case S_CONNECTED_LOOP  :
+      
+      while(!bleCommandQueue.isEmpty()){
+        processCommand(bleCommandQueue.pop());
+      }
+      
+      faderIntensity.update();
+      faderTemperature.update();
+      // Display
+      lcd.setCursor(0, 1);
+      lcdPrintNumberPadded(faderIntensity.getSetpointNormalised() * 100, 5, ' ');
+      lcd.print("%");
+      lcdPrintNumberPadded(faderTemperature.getSetpointNormalised() * 100, 6, ' ');
+      lcd.print("%");
+      /*
+      // millis per frame
+      lcd.setCursor(0, 1);
+      lcdPrintNumberPadded(thisFrameMillis - millisLastFrame, 2, ' ');
+      */
+      break;
+
   }
+
   frameCount++;
   millisLastFrame = thisFrameMillis;
+}
+
+
+// BLUETOOTH
+
+
+void queueCommand (char command, String hexstring){
+  
+  while(bleCommandQueue.count() > 20){
+    if(bleCommandQueue.peek().command == command)
+      bleCommandQueue.pop();
+  }
+  bleCommand cmd = {command, hexstring};
+  bleCommandQueue.push(cmd);
+}
+
+void processCommand (struct bleCommand cmd){
+    switch (cmd.command) {
+        case 'I':                                          
+        faderIntensity.setSetpointNormalised(cmd.hexstring.toInt()/65535.0);
+        break;
+        case 'T':                                          
+        faderTemperature.setSetpointNormalised(cmd.hexstring.toInt()/65535.0);
+        break;
+    }
+}
+
+
+extern "C" {
+
+  void RFduinoBLE_onConnect() {
+    state = S_CONNECTED_SETUP;
+    ;
+  }
+
+  void RFduinoBLE_onDisconnect() {
+    state = S_STANDALONE_SETUP;
+    ;
+  }
+
+  void RFduinoBLE_onReceive(char *data, int len) {
+    for (int i = 0; i < len; i++) {
+      bleInputStateMachine(data[i]);
+    }
+  }
+
+  void RFduinoBLE_onAdvertisement(bool start)
+  {
+    bleAdvertising = start;
+  }
+}
+
+void bleInputStateMachine(char data) {
+  switch (bleInputState) {
+    case BIS_START:                                           // wait for start byte
+      if (data == 1) {
+        bleInputState = BIS_COMMAND;
+      }
+      break;
+    case BIS_COMMAND:                                           // wait for command
+      if (data == 'I' || data == 'T' ) { // If we received a valid command
+        bleInputCommandBuf = data;                             // store it
+        bleInputHexstringBuf = "";                             // prepare to receive a hex string
+        bleInputState = BIS_DATA;
+      } else if (data != 1) {                        //Stay in state 2 if we received another 0x01
+        state = BIS_START;
+      }
+      break;
+    case  BIS_DATA:                                            // receive hex string
+      if ((data >= 'a' && data <= 'z') || (data >= 'A' && data <= 'Z') || (data >= '0' && data <= '9')) {
+        bleInputHexstringBuf = bleInputHexstringBuf + data;               // if we received a valid hex byte, add it to the end of the string
+        if (bleInputHexstringBuf.length() == 6) {               // If we have received 6 characters (24 bits) move to state 4
+          bleInputState = BIS_END;
+        }
+      } else if (data == 1) {                         // If we received another 0x01 back to state 2
+        bleInputState = BIS_COMMAND;
+      } else {
+        bleInputState = BIS_START;                           // Anything else is invalid - back to look for 0x01
+      }
+      break;
+    case BIS_END:
+      if (data == 3)                                   // 0x03=valid terminator
+      {
+        queueCommand(bleInputCommandBuf, bleInputHexstringBuf);            // We have a valid command message - process it
+            while (! RFduinoBLE.send(bleInputCommandBuf))
+      ;  // all tx buffers in use (can't send - try again later)
+        bleInputState = BIS_START;
+      } else if (data == 1) {                          // 0x01= start of new message, back to state 2
+        bleInputState = BIS_COMMAND;
+      } else {
+        bleInputState = BIS_START;                             // anything else, back to look for 0x01
+      }
+      break;
+  }
+}
+
+void setDisplayColorRGB(double r, double g, double b) {
+  pwm.setPWM(0, 0, 0xFFF - round( 0xFFF * r ));
+  pwm.setPWM(1, 0, 0xFFF - round( 0xFFF * g * 0.85));
+  pwm.setPWM(2, 0, 0xFFF - round( 0xFFF * b * 0.7));
 }
 
 // UTILS
@@ -187,17 +420,3 @@ float mapFloat(float x, float in_min, float in_max, float out_min, float out_max
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-void comm_i2c(int address, int data) {
-  Wire.beginTransmission(address);
-  Wire.write(byte(data));
-  Wire.endTransmission();
-  delay(5);
-}
-
-void send_i2c(int address, int data, int val) {
-  Wire.beginTransmission(address);
-  Wire.write(byte(data));
-  Wire.write(byte(val));
-  Wire.endTransmission();
-  delay(5);
-}
